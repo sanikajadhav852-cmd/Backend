1352ae71e08e5cf0831d4eb2a226c66ab85dba2a
Sanika Jadhav <sanikajadhav1803@gmail.com>
2026-02-22 00:53:21 +0530

    Fix CORS order

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const express = require("express");
 const cors = require("cors"); 
 const app = express();
 
+// ✅ CORS MUST BE FIRST
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Parking Fee API is running");
 });
-
-
-app.use(cors({
-  origin: "http://localhost:5173", // frontend origin
-  credentials: true
-}));
-
-
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
